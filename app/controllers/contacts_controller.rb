b739f32677e71711dc2f3645614189321105d4e3
Narendra Maharjan <narendramaharjan90s@gmail.com>
2017-01-09 00:25:23 +0000

    Added contact save path, validation process too

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,24 @@
 class ContactsController < ApplicationController
   def new
-      @contact = Contact.new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = 'Message sent'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error occured, message has not been seen.'
+      redirect_to new_contact_path
+    end
+    
   end
   
-  def create
-  end
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
+      
 end
